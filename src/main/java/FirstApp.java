76fd055138c5960b6ce33463227b9b479a49d16f
ValentinaSinitsa <svplatina7@gmail.com>
2022-11-02 20:25:53 +0300

    init commit

---
+++ src/main/java/FirstApp.java
@@ -0,0 +1,14 @@
+/*
+* Created Sinitsa on 17/03/2022
+* */
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(3);
+        System.out.println(3.1);
+        float b=2.34f, _a=2.0f;
+        _a=b*_a;
+        System.out.println(_a);
+
+    }
+}
